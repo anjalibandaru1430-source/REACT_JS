5ef45a381bb6e1f1b03352d9abad596186060ea4
anjalibandaru1430-source <anjalibandaru1430@gmail.com>
2026-04-15 21:44:39 +0530

    React

---
+++ r31/src/Message.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+class Message extends React.Component{
+    render(){
+        return <div>
+                    <h2>Message Component</h2>
+                </div>
+    }
+}
+export default Message;
