5365062c3b3221cfe87eb39a009b107e7e98e70a
anjalibandaru1430-source <anjalibandaru1430@gmail.com>
2026-05-26 11:33:41 +0530

    hooks

---
+++ Hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Message from './components/Message/Message.jsx';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Memo Hook</h1>
+      <Message/>
+    </div>
+  )
+}
+
+export default App
