427ba27686fa78b8cd0a7d5e548ebd43be27b0be
anjalibandaru1430-source <anjalibandaru1430@gmail.com>
2026-05-29 00:24:06 +0530

    rjs

--- ContactModule/src/Components/Home.jsx
+++ ContactModule/src/Components/Home.jsx
@@ -11,14 +11,3 @@ const Home = () => {
 export default Home
 
 
-/*import React from 'react'
-
-const Home = () => {
-  return (
-    <div>
-      <h2>welcome to home page</h2>
-    </div>
-  )
-}
-
-export default Home */
